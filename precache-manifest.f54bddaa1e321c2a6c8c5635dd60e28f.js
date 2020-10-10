d864ab23e98495d9784a7eba65d3bf12d92cf267
Hong-JunHyeok <edb1631@naver.com>
2020-10-10 11:55:24 +0900

    Updates

---
+++ precache-manifest.f54bddaa1e321c2a6c8c5635dd60e28f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b40c2ca362090c168b9b6a527d241d54",
+    "url": "/React_ToDoList/index.html"
+  },
+  {
+    "revision": "519e99de8dfbca4e9943",
+    "url": "/React_ToDoList/static/css/main.b926edf0.chunk.css"
+  },
+  {
+    "revision": "57b382dce3672a579f47",
+    "url": "/React_ToDoList/static/js/2.50f6336a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React_ToDoList/static/js/2.50f6336a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "519e99de8dfbca4e9943",
+    "url": "/React_ToDoList/static/js/main.8849da5a.chunk.js"
+  },
+  {
+    "revision": "13339e5810319dfedbe5",
+    "url": "/React_ToDoList/static/js/runtime-main.750cbc1a.js"
+  }
+]);
